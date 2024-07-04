d62759bd9cfbe29eb39d51f4520f8a8e62b17c7b
EmekaIwuagwu <45670176+EmekaIwuagwu@users.noreply.github.com>
2024-07-04 16:52:19 +0100

    Update database.go

--- database/database.go
+++ database/database.go
@@ -11,7 +11,7 @@ var DB *sql.DB
 
 func ConnectDB() {
     var err error
-    DB, err = sql.Open("mysql", "root:@tcp(localhost:3306)/banking_db")
+    DB, err = sql.Open("mysql", "avnadmin:AVNS_QK_5Qr2G4q3mK8QSid-@tcp(mysql-1d10df7f-ozizichristopher712-96e0.f.aivencloud.com:11745)/banking_db")
     if err != nil {
         log.Fatal(err)
     }
